dbe5736ec2cbbe209041b1b7c532660a7f77e7a5
Mighty <tosha.hashimov@googlemail.com>
2021-10-24 18:25:00 +0100

    basic functionality works. Still needs work

--- script/script.js
+++ script/script.js
@@ -1,60 +1,117 @@
-let zero = document.querySelector(".zero");
-let one = document.querySelector(".one");
-let two = document.querySelector(".two");
-let three = document.querySelector(".three");
-let four = document.querySelector(".four");
-let five = document.querySelector(".five");
-let six = document.querySelector(".six");
-let seven = document.querySelector(".seven");
-let eight = document.querySelector(".eight");
-let nine = document.querySelector(".nine");
-let multi = document.querySelector(".multi");
-let dev = document.querySelector(".dev");
-let plus = document.querySelector(".plus");
-let minus = document.querySelector(".minus");
-let reset = document.querySelector(".reset");
-let period = document.querySelector(".period");
+let numbers = document.querySelectorAll(".number");
+let operand = document.querySelectorAll(".operand");
+let mainNumber = document.querySelector(".mainNumber");
+let secondaryNumber = document.querySelector(".secondaryNumber");
 
-zero.addEventListener('click', calculation);
-one.addEventListener('click', calculation);
-two.addEventListener('click', calculation);
-three.addEventListener('click', calculation);
-four.addEventListener('click', calculation);
-five.addEventListener('click', calculation);
-six.addEventListener('click', calculation);
-seven.addEventListener('click', calculation);
-eight.addEventListener('click', calculation);
-nine.addEventListener('click', calculation);
-multi.addEventListener('click', calculation);
-dev.addEventListener('click', calculation);
-plus.addEventListener('click', calculation);
-minus.addEventListener('click', calculation);
-reset.addEventListener('click', calculation);
-period.addEventListener('click', calculation);
+for (i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', updateNumbers);
+} 
 
-let arg1 = 0;
-let arg2 = 0;
-let arg3 = 0;
-let arg4 = 0;
+for (i = 0; i < operand.length; i++) {
+    operand[i].addEventListener('click', decodeOperand);
+}
 
+let arg0 = [];
+let arg1 = [];
+let num0 = 0;
+let num1 = 0;
+let result = 0;
 
-let result = 0
+let hasCalculated = false;
+let add = false;
+let sub = false;
+let divide = false;
+let multiply = false;
 
-function calculation() {
+function updateNumbers() {
+    if (!hasCalculated) {
+        arg0.push(this.value);
+        num0 = arg0.join('');
+        showCurrentNumber();
+    } else {
+        arg1.push(this.value);
+        num1 = arg1.join('');
+        showCurrentNumber(); 
+    }
 }
 
 function addition() {
-
+    hasCalculated = true;
+    add = true;
 }
 
 function subtraction() {
-
+    hasCalculated = true;
+    sub = true;
 }
 
 function division() {
-
+    hasCalculated = true;
+    divide = true;
 }
 
 function multiplication() {
+    hasCalculated = true;
+    multiply = true;
+}
 
+function showCurrentNumber() {
+    if (!hasCalculated) {
+        mainNumber.textContent = parseInt(num0);
+    } else {
+        secondaryNumber.textContent = parseInt(num0);
+        mainNumber.textContent = parseInt(num1);
+    }
+}
+
+function showResult() {
+    if (add) {
+        result = parseInt(num0) + parseInt(num1);
+    } else if (sub) {
+        result = parseInt(num0) - parseInt(num1);
+    } else if (divide) {
+        result = parseInt(num0) / parseInt(num1);
+    } else if (multiply) {
+        result = parseInt(num0) * parseInt(num1);
+    }
+
+    mainNumber.textContent = result;
+}
+
+function resetCalculator() {
+    arg0 = [];
+    arg1 = [];
+    num0 = 0;
+    num1 = 0;
+    result = 0;
+    hasCalculated = false;
+    add = false;
+    sub = false;
+    divide = false;
+    multiply = false;
+    mainNumber.textContent = 0;
+    secondaryNumber.textContent = 0;
+}
+
+function decodeOperand() {
+    switch (this.value) {
+        case "+":
+            addition();
+            break;
+        case "-":
+            subtraction();
+            break;
+        case "*":
+            multiplication();
+            break;
+        case "/":
+            division();
+            break;
+        case "=":
+            showResult();
+            break;
+        case"AC":
+            resetCalculator();
+            break;
+    }
 }
