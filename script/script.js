d0757d4c9ed3b23b8f17e8618f61ba41dd3a14be
Mighty <tosha.hashimov@googlemail.com>
2021-10-24 13:06:30 +0100

    first commit

---
+++ script/script.js
@@ -0,0 +1,60 @@
+let zero = document.querySelector(".zero");
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+let seven = document.querySelector(".seven");
+let eight = document.querySelector(".eight");
+let nine = document.querySelector(".nine");
+let multi = document.querySelector(".multi");
+let dev = document.querySelector(".dev");
+let plus = document.querySelector(".plus");
+let minus = document.querySelector(".minus");
+let reset = document.querySelector(".reset");
+let period = document.querySelector(".period");
+
+zero.addEventListener('click', calculation);
+one.addEventListener('click', calculation);
+two.addEventListener('click', calculation);
+three.addEventListener('click', calculation);
+four.addEventListener('click', calculation);
+five.addEventListener('click', calculation);
+six.addEventListener('click', calculation);
+seven.addEventListener('click', calculation);
+eight.addEventListener('click', calculation);
+nine.addEventListener('click', calculation);
+multi.addEventListener('click', calculation);
+dev.addEventListener('click', calculation);
+plus.addEventListener('click', calculation);
+minus.addEventListener('click', calculation);
+reset.addEventListener('click', calculation);
+period.addEventListener('click', calculation);
+
+let arg1 = 0;
+let arg2 = 0;
+let arg3 = 0;
+let arg4 = 0;
+
+
+let result = 0
+
+function calculation() {
+}
+
+function addition() {
+
+}
+
+function subtraction() {
+
+}
+
+function division() {
+
+}
+
+function multiplication() {
+
+}
